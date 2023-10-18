8fdfd1aef8e93ccf03f188d56e1170ca9e503818
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-10-18 20:42:35 +1100

    Started KDTree classifier

---
+++ src/kDTreeClassifier.ts
@@ -0,0 +1,15 @@
+export const KDTreeClasifier = () => {
+    const classify = async (query) => {
+        /*
+               const { text } = query;
+               const { dataSet } = query as ClassifyDataSetQuery;
+               const modelsFactory = ModelsFactory();
+               const model = modelsFactory.create("dataPointFeature");
+               const predictionModel = await model.train(dataSet);
+               const { predictions } = await predictionModel.predict(text) as FeatureClassifyResponse;
+               return { predictions } as FeatureClassifyResponse;
+           }*/
+        //return { classify } as ClassifierBase;
+        return null;
+    }
+}
