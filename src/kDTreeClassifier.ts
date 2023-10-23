b668d6b6d97fe84cb54fd675c0c7ddb506ab924e
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-10-24 06:58:52 +1100

    Start KDTreeClassifier

--- src/kDTreeClassifier.ts
+++ src/kDTreeClassifier.ts
@@ -1,15 +1,16 @@
+import { ClassifyQuery } from "classification-server/models/classifyQuery";
+import { ClassifierBase } from "classification-server/modules/classifiers/classifierBase"
+
+
 export const KDTreeClasifier = () => {
-    const classify = async (query) => {
-        /*
-               const { text } = query;
-               const { dataSet } = query as ClassifyDataSetQuery;
-               const modelsFactory = ModelsFactory();
-               const model = modelsFactory.create("dataPointFeature");
-               const predictionModel = await model.train(dataSet);
-               const { predictions } = await predictionModel.predict(text) as FeatureClassifyResponse;
-               return { predictions } as FeatureClassifyResponse;
-           }*/
-        //return { classify } as ClassifierBase;
-        return null;
+    const classify = async (query: ClassifyQuery) => {
+        const { text } = query;
+        const { dataSet } = query as ClassifyDataSetQuery;
+        const modelsFactory = ModelsFactory();
+        const model = modelsFactory.create("dataPointFeature");
+        const predictionModel = await model.train(dataSet);
+        const { predictions } = await predictionModel.predict(text) as FeatureClassifyResponse;
+        return { predictions } as FeatureClassifyResponse;
     }
+    return { classify } as ClassifierBase;
 }
