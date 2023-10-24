dd0aee319de9905a520a2af3ab23523f84c15967
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-10-24 18:55:27 +1100

    More work on importing types from classification-server/types

--- src/kDTreeClassifier.ts
+++ src/kDTreeClassifier.ts
@@ -1,16 +1,20 @@
-import { ClassifyQuery } from "classification-server/models/classifyQuery";
-import { ClassifierBase } from "classification-server/modules/classifiers/classifierBase"
+import { ClassifyQuery } from "classification-server/types/classifyQuery";
+import { ClassifyDataSetQuery } from "classification-server/types/classifyDataSetQuery";
+import { FeatureClassifyResponse } from "classification-server/types/featureClassifyResponse";
 
+import { ClassifierBase } from "classification-server/modules/classifiers/classifierBase";
 
 export const KDTreeClasifier = () => {
-    const classify = async (query: ClassifyQuery) => {
-        const { text } = query;
-        const { dataSet } = query as ClassifyDataSetQuery;
-        const modelsFactory = ModelsFactory();
-        const model = modelsFactory.create("dataPointFeature");
-        const predictionModel = await model.train(dataSet);
-        const { predictions } = await predictionModel.predict(text) as FeatureClassifyResponse;
-        return { predictions } as FeatureClassifyResponse;
-    }
-    return { classify } as ClassifierBase;
-}
+  const classify = async (query: ClassifyQuery) => {
+    const { text } = query;
+    const { dataSet } = query as ClassifyDataSetQuery;
+    const modelsFactory = ModelsFactory();
+    const model = modelsFactory.create("dataPointFeature");
+    const predictionModel = await model.train(dataSet);
+    const { predictions } = (await predictionModel.predict(
+      text
+    )) as FeatureClassifyResponse;
+    return { predictions } as FeatureClassifyResponse;
+  };
+  return { classify } as ClassifierBase;
+};
