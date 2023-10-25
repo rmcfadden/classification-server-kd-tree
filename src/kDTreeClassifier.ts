a8535b8fdaf94ca2412350d71592bcfbccdfe1e4
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-10-25 22:04:43 +1100

    Testing new imports

--- src/kDTreeClassifier.ts
+++ src/kDTreeClassifier.ts
@@ -1,20 +1,17 @@
-import { ClassifyQuery } from "classification-server/types/classifyQuery";
-import { ClassifyDataSetQuery } from "classification-server/types/classifyDataSetQuery";
-import { FeatureClassifyResponse } from "classification-server/types/featureClassifyResponse";
+import { ClassifyQuery, ClassifyDataSetQuery, FeatureClassifyResponse, FeaturePrediction, ClassifierBase } from "classification-server/types";
 
-import { ClassifierBase } from "classification-server/modules/classifiers/classifierBase";
 
-export const KDTreeClasifier = () => {
-  const classify = async (query: ClassifyQuery) => {
-    const { text } = query;
-    const { dataSet } = query as ClassifyDataSetQuery;
-    const modelsFactory = ModelsFactory();
-    const model = modelsFactory.create("dataPointFeature");
-    const predictionModel = await model.train(dataSet);
-    const { predictions } = (await predictionModel.predict(
-      text
-    )) as FeatureClassifyResponse;
-    return { predictions } as FeatureClassifyResponse;
-  };
-  return { classify } as ClassifierBase;
+export const KDTreeClassifier = () => {
+    const classify = async (query: ClassifyQuery) => {
+        const { text } = query;
+        const { dataSet } = query as ClassifyDataSetQuery;
+        //const modelsFactory = ModelsFactory();
+        //const model = modelsFactory.create("dataPointFeature");
+        //const predictionModel = await model.train(dataSet);
+        //const { predictions } = (await predictionModel.predict(
+        //  text
+        //)) as FeatureClassifyResponse;
+        return { predictions: [] as FeaturePrediction[] } as FeatureClassifyResponse;
+    };
+    return { classify } as ClassifierBase;
 };
---
+++ src/kdTreeModel.ts
@@ -0,0 +1,23 @@
+import { DataSet, PredictionResult, NDDataPointFeature, FeaturePredictionResult, NDDataPoint, ModelBase } from "classification-server/types";
+import { NDDataPointConverter } from "classification-server/converters";
+
+export const KdTreeModel = (dataFeatures: NDDataPointFeature[]) => {
+    const predict = async (input: string): Promise<PredictionResult> => {
+        const { values }: NDDataPoint = NDDataPointConverter().parse(input);
+        const sortedFeatures = [...dataFeatures].sort(
+            (a: NDDataPointFeature, b: NDDataPointFeature) =>
+                Math.hypot(...a.values.map((a1, i) => values[i] - a1)) -
+                Math.hypot(...b.values.map((b1, i) => values[i] - b1))
+        );
+        // TODO: how do I determine probability based on distance (posterior probability?)
+        return {
+            predictions: sortedFeatures.map(({ feature }) => ({
+                feature,
+                probability: 100,
+            })),
+        } as FeaturePredictionResult;
+    };
+    const train = async (dataSet: DataSet) =>
+        KdTreeModel(dataSet.items as NDDataPointFeature[]);
+    return { predict, train } as ModelBase;
+}
