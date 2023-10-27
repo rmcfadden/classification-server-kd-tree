44d18b76ce787e781747b16e4372ad2e14c1b5e6
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-10-27 16:21:56 +1100

    Model Tests

--- tests/kTreeModel.test.ts
+++ tests/kTreeModel.test.ts
@@ -1,7 +1,19 @@
+import { NDDataPointFeature, FeaturePredictionResult } from "classification-server/types";
+
 import { KdTreeModel } from "../src/kdTreeModel";
 test("predict", async () => {
-    const { predict } = KdTreeModel([]);
-
-    const predictions = await predict("1");
-    console.log("RESULT", predictions);
+    const dataPoints: NDDataPointFeature[] = [
+        { values: [0, 0], feature: "fruit" },
+        { values: [1, 1], feature: "fruit" },
+        { values: [3, 3], feature: "vegetable" },
+        { values: [4, 4], feature: "vegetable" },
+        { values: [-2, -2], feature: "grain" },
+    ];
+    const { predict } = KdTreeModel(dataPoints);
+    const {
+        predictions: [prediction],
+    }: FeaturePredictionResult = (await predict(".75,.80")) as FeaturePredictionResult;
+    const { feature, probability } = prediction;
+    expect(feature).toBe("fruit");
+    expect(probability).toBe(100);
 });
