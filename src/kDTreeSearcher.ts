273663a6251a4966e6eb8cab4bd0a1017c3f3b16
Ryan McFadden <ryanpmcfadden@gmail.com>
2023-11-25 20:16:53 +1100

    Searcher

--- src/kDTreeSearcher.ts
+++ src/kDTreeSearcher.ts
@@ -4,6 +4,8 @@ import { KDTreeNode } from "./kDTreeNode";
 export const KDTreeSearcher = () => {
     const search = (tree: KDTreeNode, dataPoint: NDDataPoint) => {
         if (!tree) return undefined;
+
+
     }
 
     return { search };
